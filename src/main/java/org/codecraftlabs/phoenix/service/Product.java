c07354f985ff196d2485d3fbdb749cbc71bdbe88
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2021-08-11 01:15:29 +0200

    initial code

---
+++ src/main/java/org/codecraftlabs/phoenix/service/Product.java
@@ -0,0 +1,50 @@
+package org.codecraftlabs.phoenix.service;
+
+public class Product {
+    private String id;
+    private String title;
+    private String description;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    private String imagePath;
+    private double unitPrice;
+}
---
+++ src/test/java/org/codecraftlabs/phoenix/PhoenixApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codecraftlabs.phoenix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoenixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
