27aaed47d6ac1e6a971cac60c4bdc4c58c997c48
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2021-08-31 21:38:23 +0200

    Add Spring cloud config support

--- src/main/java/org/codecraftlabs/phoenix/service/ProductCatalogService.java
+++ src/main/java/org/codecraftlabs/phoenix/service/ProductCatalogService.java
@@ -1,5 +1,6 @@
 package org.codecraftlabs.phoenix.service;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@RefreshScope
 public class ProductCatalogService {
     private static final Map<String, Product> productCatalog = new HashMap<>();
 
